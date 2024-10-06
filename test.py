e663d6b3856927365fe740c7513c71e4741bce0c
forker <youhaili59@outlook.com>
2024-10-06 13:57:44 -0400

    changed

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+def divide_numbers(a, b):
+    # Potential division by zero (SonarQube will detect this as a bug)
+    return a / b
+
+def incorrect_condition(x):
+    # Bug: This condition is always false (SonarQube will detect this logic flaw)
+    if x < 0 and x > 10:
+        print("This will never be printed")
+
 def __main__():
     print("Helloa, World!")
     print("Helloa, World!")
@@ -8,3 +17,5 @@ def __main__():
     print("Hellob, World!")
     prit("Hellob, World!")
     prit("Hellob, World!")
+    divide_numbers(1, 0)
+    incorrect_condition(5)
