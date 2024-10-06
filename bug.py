27020d5ccdb219f0c86d3e81c77a150283243dde
forker <youhaili59@outlook.com>
2024-10-06 14:12:03 -0400

    changed

--- bug.py
+++ bug.py
@@ -1,6 +1,7 @@
 def example_no_file_closing():
     f = open('example1.txt', 'r')
-    data = f.read()  # SonarQube: Resource (file) should be properly closed
+    data = f.read()
+    List<int> shit = new ArrayList<int>(); # SonarQube: Resource (file) should be properly closed
     return data
 
 def __main__():
