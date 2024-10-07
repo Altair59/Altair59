dde4c3e5e1019f37b42e01c2eeaf36a6e387b3be
forker <youhaili59@outlook.com>
2024-10-06 23:14:17 -0400

    changed

--- WordCount.java
+++ WordCount.java
@@ -32,7 +32,7 @@ public class WordCount {
                 Reporter reporter) throws IOException {
             int sum = 0;
             while (values.hasNext()) {
-                sum += values.next().get() * 3;
+                sum += values.next().get() * g;
             }
             output.collect(key, new IntWritable(sum));
         }
